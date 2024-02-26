32e6986b19b3719f302ea507391b435f7809dbef
gspark_ketech <123606513+pgs931016@users.noreply.github.com>
2024-02-26 15:53:40 +0900

    Add files via upload

---
+++ Integrated_files.m
@@ -0,0 +1,84 @@
+clear; clc; close all
+
+
+%% USE PROCESSED DATA
+for i = 1:length()
+
+folder = 'G:\공유 드라이브\BSL_Data2\HNE_AgingDOE_Raw\HNE_FCC\4CPD 1C (25-42)\25degC';
+
+files = dir(fullfile(folder,'*.mat')); % .mat
+
+
+for n = 1:size(files,1)
+
+% identify cell number
+% 한 폴더에 셀 2개 에 대한 파일 이 있음
+    numcell=regexp(files(n).name,'\d+','match');
+    files(n).cellnum = str2num(numcell{end-1})                                                          ;
+
+
+end
+
+
+% 몇개의 셀이 있는지 확인, 셀넘버 가져오기)
+cellnum_list = [files.cellnum]';
+cellnum_list = unique(cellnum_list);
+
+
+% 각 셀당 당 (2개)
+for i = 1:length(cellnum_list)
+    
+
+        cellfile_list = files([files.cellnum]==cellnum_list(i));
+
+        
+    
+        for j = 1:size(cellfile_list,1) % 각 셀의 실험 파일들 (aging N개, + RPT 개 )
+
+            
+            % identify experiment number "RPT_N" --> N "AgingN" --> N
+            allnum = regexp(cellfile_list(j).name,'\d+','match');
+            cellfile_list(j).expnum = str2num(allnum{end});
+
+            % identify if RPT or Aging
+            if ~isempty(regexp(cellfile_list(j).name,'Aging','match'))
+        
+                cellfile_list(j).rptflag = 0;
+                
+            elseif  ~isempty(regexp(cellfile_list(j).name,'RPT','match'))
+        
+                cellfile_list(j).rptflag = 1;
+        
+            end
+
+
+            % sort by expnum --> rptflag(rev)
+                % (1) sort --> sorted ind
+                % (2) rearrange cellfile_list 
+                %           cellfile_list = cellfile_list(sorted_ind)
+            
+            % merge (append) mat files AND SAVE
+                % (a) save ('',.'','-append'(
+                % (b) merge 명령어 있는지 찾아보
+
+        end
+end
+
+%% Helper Function to Recursively Get All Files
+function fileList = AllFiles(dirName, filePattern)
+    dirData = dir(dirName);      % Get the data for the current directory
+    dirIndex = [dirData.isdir];  % Find the index for directories
+    fileList = {dirData(~dirIndex).name}';  % Get a list of the files
+    
+    if ~isempty(fileList)
+        fileList = cellfun(@(x) fullfile(dirName,x),...  % Convert to full path
+            fileList,'UniformOutput',false);
+    end
+    subDirs = {dirData(dirIndex).name};  % Get a list of sub-directories
+    validIndex = ~ismember(subDirs,{'.','..'});  % Find index of sub-directories excluding '.' and '..'
+
+    for iDir = find(validIndex)                  % Loop over valid sub-directories
+        nextDir = fullfile(dirName,subDirs{iDir}); % Get the sub-directory path
+        fileList = [fileList; getAllFiles(nextDir, filePattern)];  % Recurse
+    end
+end
